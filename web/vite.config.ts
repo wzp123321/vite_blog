53688529bf0efe9fb5a727c5808af346ee3c39d4
wzp123321 <wanzhipengx@163.com>
2021-06-21 22:18:46 +0800

    新增代码

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  server: {
+    port: 9526,
+    open: true,
+    hmr: true,
+    proxy: {
+      "^/api": {
+        target: "http://localhost:9999/blogWeb",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
